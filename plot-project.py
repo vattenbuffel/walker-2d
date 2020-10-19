85ea373e1cecabfa9d95a42606839b96da11ccc5
Noa <noal@student.chalmers.se>
2020-10-19 14:01:12 +0200

    Update plot code

--- plot-project.py
+++ plot-project.py
@@ -2,20 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
-test_rewards = np.array([1,2,3])
-distance_travlled = np.array([4,5,7])
+test_rewards = []
+distance_travlled = []
+with open("distance_travelled.npy", 'rb') as f:
+    distance_travlled = np.load(f)
+with open("test_rewards.npy", 'rb') as f:
+    test_rewards = np.load(f)
 
 
+# Plot reward
+plt.subplot(2,1,1)
 plt.plot(test_rewards, color='b')
 reward_patch = mpatches.Patch(color='b', label='Reward')
+plt.legend(handles=[reward_patch])
+plt.xlabel("Training-iteration")
+plt.ylabel("Reward")
 
+# Plot distance
+plt.subplot(2,1,2)
 plt.plot(distance_travlled, color='r')
 distance_travelled_patch = mpatches.Patch(color='r', label='Distance travelled')
-
-plt.legend(handles=[reward_patch, distance_travelled_patch])
+plt.legend(handles=[distance_travelled_patch])
 plt.xlabel("Training-iteration")
-plt.ylabel("Value")
+plt.ylabel("Reward")
+
 
 plt.show()
-balle = 5
 print("done")
